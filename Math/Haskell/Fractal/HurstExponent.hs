18709134ee4e0ae95d95163313b74f0a512e9ae1
m00nlight <dot_wangyushi@yeah.net>
2014-12-30 07:15:16 -0800

    can handle list which length is not exponent of 2

--- Math/Haskell/Fractal/HurstExponent.hs
+++ Math/Haskell/Fractal/HurstExponent.hs
@@ -13,8 +13,8 @@ the <http://en.wikipedia.org/wiki/Rescaled_range Rescale_Range> methods.
 Briefly speaking, the function will calculate the R/S score of the time
 series data with different chunck size, and using linear regression to
 calculate the slope in log-log coordinates of the R/S average value and
-chunk size. So the total input time series data should be of 2^n where
-n is an integer.
+chunk size. The series data had better be of length of exponent of 2,
+but it does not need to satisfy.
 
 More details of how to calculate the Hurst Exponent, please see the
 <http://en.wikipedia.org/wiki/Hurst_exponent wikipedia> page .
@@ -49,17 +49,21 @@ hurstRS xs = if s ≡ 0.0 then 1.0 else r / s
 {-|
   The 'hurstExponent' function is used to estimate the
   <http://en.wikipedia.org/wiki/Hurst_exponent Hurst_Expoennt> of a
-  time series data (v₁, v₂, ..., vₙ). n should be exponent of 2.
+  time series data (v₁, v₂, ..., vₙ). n had better be exponent of 2.
+  If n is not the exponent of 2, we will take as many elements as
+  possible from the tail of the list to form an list of length of 2^m.
   The value  can be late used to calculate the fractal dimension of
   the time series data.
 -}
 hurstExponent ∷ [Double] → Result
 hurstExponent vs = snd res
     where
-      n = length vs
+      len = length vs
+      n = last $ takeWhile (<len) [2^i | i ← [1..]]
+      vs' = drop (len - n) vs
       chunkSizes = nestWhile (>4) (\ x → x `div` 2) n
       help ∷ [Double] → Int → (Double, Double)
       help vs size = (log (average $ map hurstRS (chunksOf size vs)) / log 2.0,
                       log (fromIntegral size) / log 2.0)
-      tmp = map (\ x → help vs x) chunkSizes
+      tmp = map (\ x → help vs' x) chunkSizes
       res = linearRegression (map snd tmp) (map fst tmp)
